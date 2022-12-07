24f9e0add9f5625d2070a70693eeac11c4d7821f
Davide Filosa <davideclaypool@gmail.com!>
2022-12-07 12:51:12 +0100

    Created Overall App Layout

--- webapp/home.py
+++ webapp/home.py
@@ -3,16 +3,34 @@ import justpy as jp
 class Home:
     path = '/'
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        main = jp.Div(a=wp, classes="bg-gray-200 h-screen w-screen")
-        jp.Div(a=main, text='Instant Dictionary Web App', classes='text-3xl m-3 w-screen')
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout, classes="bg-primary text-white")
+        toolbar = jp.QToolbar(a=header)
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode='left', border=True)
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon='menu', click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text='Instant Dictionary')
+        container = jp.QPageContainer(a=layout)
+        main = jp.Div(a=container, classes="bg-gray-200 h-screen w-screen p-2")
+        jp.Div(a=main, text='Instant Dictionary Web App', classes='text-3xl m-2 w-screen')
         jp.Div(a=main, text='''A web app that lets users type in 
         a term in a text box and returns the English definition
         of that term instantly as soon as the user has finished typing.
         The web app consists of a website with a navigation menu, a 
         Home, Dictionary, and About page.
                 ''', classes='text-lg m-2')
+
         return wp
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
+
+
+
 
 
